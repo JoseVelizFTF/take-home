af34f01f703fd59124e9bc06e7d85d05a5b10b25
JoseVelizFTF <jose.veliz@fulltimeforce.com>
2020-10-08 09:56:06 -0500

    pipe time ago added

--- src/app/features/commit/commit.module.ts
+++ src/app/features/commit/commit.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 import { CommitRoutingModule } from './commit-routing.module';
 import { CommitListComponent } from './components/commit-list/commit-list.component';
 import { CommitItemComponent } from './components/commit-item/commit-item.component';
 
 @NgModule({
-  declarations: [CommitListComponent, CommitItemComponent],
+  declarations: [CommitListComponent, CommitItemComponent, TimeAgoPipe],
   imports: [CommonModule, CommitRoutingModule, HttpClientModule],
   exports: [CommitListComponent],
 })
